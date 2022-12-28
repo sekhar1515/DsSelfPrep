3b3e23b936f03426fd794e4195609d1fcead0aee
sekharvirat <sekharreddy1515@gmail.com>
2022-12-28 08:10:34 +0530

    Bubble sort implementation

--- SortingAlgorithms/src/Main.java
+++ SortingAlgorithms/src/Main.java
@@ -1,9 +1,10 @@
+import sortingalgor.BubbleSort;
 import sortingalgor.SelectionSort;
 
 public class Main {
     public static void main(String[] args) {
 //        System.out.println("Hello world!");
-        SelectionSort selectionSort = new SelectionSort();
-        selectionSort.selectionSort(new int[]{32,3556,24346,12,55567});
+        BubbleSort bs = new BubbleSort();
+        bs.bubbleSort(new int[] {33,-10,24,22,222,222,22,2});
     }
 }
---
+++ SortingAlgorithms/src/sortingalgor/BubbleSort.java
@@ -0,0 +1,29 @@
+package sortingalgor;
+
+import java.util.Arrays;
+
+public class BubbleSort
+{
+//    The bubble sort works in such a way that we always swap consecutive elements and then we move forward.
+//     TC : O(N^2)
+    public void bubbleSort(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            boolean isSwapped = false;
+            for(int j = i+1; j < arr.length; j++){
+                if(arr[i] > arr[j]){
+                    swapValues(arr,i,j);
+                    isSwapped = true;
+                }
+            }
+            if(!isSwapped)
+                break;
+        }
+        Arrays.stream(arr).forEach((ele) -> {System.out.println(ele);});
+    }
+
+    private void swapValues(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
